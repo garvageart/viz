44bbc980925a175469a2f8fe0ed4e4e88bae0a40
Les <45718863+garvageart@users.noreply.github.com>
2025-06-03 13:03:34 +0200

    redirect if no provider found

--- viz/src/routes/signin/oauth/+page.svelte
+++ viz/src/routes/signin/oauth/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
 	import { sendOAuthParams } from "$lib/auth/auth_methods";
 
 	const provider: string | null = new URL(location.href).searchParams.get("provider");
+
+	if (!provider?.trim()) {
+		goto("/");
+	}
+
 	sendOAuthParams(provider);
 </script>
 
