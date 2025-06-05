22e48cc04fc4422d69d99147bb570dfc9e7748c7
Les <45718863+garvageart@users.noreply.github.com>
2025-06-05 15:31:07 +0200

    Move header to individual component

---
+++ viz/src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+
+<header>
+	<a id="viz-title" href="/">viz</a>
+</header>
+
+<slot></slot>
+
+<style>
+	header {
+		background-color: var(--imag-blue-100);
+		height: 2.5em;
+		padding: 0.5em 1.5em;
+		display: flex;
+		align-items: center;
+		border-bottom: 1px solid var(--imag-blue-20);
+	}
+
+	#viz-title {
+		font-family: var(--imag-code-font);
+		font-weight: 700;
+		font-size: 2em;
+	}
+</style>
--- viz/src/routes/+layout.svelte
+++ viz/src/routes/+layout.svelte
@@ -1,26 +1,8 @@
 <script>
+	import Header from "$lib/components/Header.svelte";
 	import "./main.scss";
 </script>
 
-<header>
-	<a id="viz-title" href="/">viz</a>
-</header>
+<Header></Header>
 
 <slot></slot>
-
-<style>
-	header {
-		background-color: var(--imag-blue-100);
-		height: 2.5em;
-		padding: 0.5em 1.5em;
-		display: flex;
-		align-items: center;
-		border-bottom: 1px solid var(--imag-blue-20);
-	}
-
-	#viz-title {
-		font-family: var(--imag-code-font);
-		font-weight: 700;
-		font-size: 2em;
-	}
-</style>
