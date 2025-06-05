76990f5b5ee807b3f59b0b75cd11ab97849dd9a4
Les <45718863+garvageart@users.noreply.github.com>
2025-06-05 14:59:27 +0200

    add close icon

---
+++ viz/src/lib/components/CloseIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { onclick, id }: { onclick: () => void; id?: string } = $props();
+</script>
+
+<button {id} {onclick}>
+	<span class="material-symbols-sharp"> close </span>
+</button>
+
+<style>
+	button {
+		height: max-content;
+	}
+</style>
