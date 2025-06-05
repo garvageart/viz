c1d43fa496cbca6372d384975ebed461c3b51c58
Les <45718863+garvageart@users.noreply.github.com>
2025-06-05 19:06:35 +0200

    refactor server launch logging

--- main.go
+++ main.go
@@ -2,18 +2,8 @@ package main
 
 import (
 	"imagine/db"
-	// "imagine/log"
 )
 
 func main() {
-	// handlers := log.SetupLogHandlers()
-	// logger := log.CreateLogger(handlers)
-
-	// logger.Info("This is an info message")
-	// logger.Error("This is an error message")
-	// logger.Debug("This is a debug message")
-	// logger.Warn("This is a warning message")
-
-	// fmt.Println(os.)
 	db.Initis()
 }
