f932db673037ba310d1ba8ae5220069aabcf6a52
Les <45718863+garvageart@users.noreply.github.com>
2025-06-10 15:07:02 +0200

    replace close icon symbol

---
+++ viz/src/lib/components/SidebarCloseIcon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { sidebar } from "$lib/states/index.svelte";
+	import MaterialIcon from "./MaterialIcon.svelte";
+
+	let { onclick, id, direction }: { onclick: () => void; id?: string; direction?: "left" | "right" } = $props();
+	let iconDirection = $state(direction || "right");
+	$effect(() => {
+		if (sidebar.open) {
+			iconDirection = "left";
+		} else {
+			iconDirection = "right";
+		}
+	});
+</script>
+
+<button {id} {onclick}>
+	<MaterialIcon iconName="chevron_{iconDirection}" />
+</button>
+
+<style>
+	button {
+		height: max-content;
+	}
+</style>
