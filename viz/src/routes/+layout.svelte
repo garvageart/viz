e63421407e3040293e4ccbd631a7efb7951bee09
Les <45718863+garvageart@users.noreply.github.com>
2025-06-13 00:38:33 +0200

    add manrope to layout file

--- viz/src/routes/+layout.svelte
+++ viz/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from "$lib/components/Header.svelte";
 	import "$lib/styles/scss/main.scss";
+	import "@fontsource-variable/manrope";
 
 	let { children } = $props();
 </script>
