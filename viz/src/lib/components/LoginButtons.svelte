deedc7a30a02917103b4a56738cd7d8c9e9bfe79
Les <45718863+garvageart@users.noreply.github.com>
2025-06-27 22:50:19 +0200

    remove login state

--- viz/src/lib/components/LoginButtons.svelte
+++ viz/src/lib/components/LoginButtons.svelte
@@ -1,28 +1,24 @@
 <script lang="ts">
 	import { authServerURL } from "$lib/auth/auth_methods";
-	import { login } from "$lib/states/index.svelte";
 	import Button from "./Button.svelte";
 
-	const loginState = login.state;
 </script>
 
-{#if !loginState}
-	<div id="login-buttons">
-		<Button
-			id="oauth-sign_in-button-google"
-			onclick={() => {
-				location.href = `${authServerURL}/oauth?provider=google`;
-			}}
-		>
-			Sign in with Google
-		</Button>
-		<Button
-			id="oauth-sign_in-button-github"
-			onclick={() => {
-				location.href = `${authServerURL}/oauth?provider=github`;
-			}}
-		>
-			Sign in with GitHub
-		</Button>
-	</div>
-{/if}
+<div id="login-buttons">
+	<Button
+		id="oauth-sign_in-button-google"
+		onclick={() => {
+			location.href = `${authServerURL}/oauth?provider=google`;
+		}}
+	>
+		Sign in with Google
+	</Button>
+	<Button
+		id="oauth-sign_in-button-github"
+		onclick={() => {
+			location.href = `${authServerURL}/oauth?provider=github`;
+		}}
+	>
+		Sign in with GitHub
+	</Button>
+</div>
