18cc575209c34bf4e5b80fcd955a6f68b7f5ceb4
Les <45718863+garvageart@users.noreply.github.com>
2025-07-07 19:45:16 +0200

    adjust default button component styles

--- viz/src/lib/components/Button.svelte
+++ viz/src/lib/components/Button.svelte
@@ -16,14 +16,15 @@
 		font-size: 1em;
 		letter-spacing: 0.02em;
 		height: max-content;
-		background-color: var(--imag-blue-80);
+		background-color: var(--imag-blue-90);
 		border: none;
-		margin: 0.5em;
-		padding: 0.5em 1em;
+		margin: 0.5em 1em;
+		padding: 0.8em 1em;
 		display: inline-block;
 		text-align: center;
 		position: relative;
 		transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+		border-radius: 100px;
 
 		&:hover {
 			background-color: var(--imag-blue-100);
