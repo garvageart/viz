ebb1daa4d70423aba9b92f984a736ab299e8eca9
Les <45718863+garvageart@users.noreply.github.com>
2025-07-08 04:08:27 +0200

    link to timezone website

--- viz/src/lib/components/panels/workspace/generic/EvenMoreDifferent.svelte
+++ viz/src/lib/components/panels/workspace/generic/EvenMoreDifferent.svelte
@@ -1,3 +1,14 @@
+<script>
+	const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+</script>
+
 <div id="viz-test_container">
-	The current timezone is <strong>{Intl.DateTimeFormat().resolvedOptions().timeZone}</strong>
+	The current timezone is
+	<a
+		aria-label="timezone"
+		target="_blank"
+		href="https://www.zeitverschiebung.net/en/timezone/{tz.toLowerCase().split('/')[0]}--{tz.toLowerCase().split('/')[1]}"
+	>
+		<strong>{Intl.DateTimeFormat().resolvedOptions().timeZone}</strong>
+	</a>
 </div>
