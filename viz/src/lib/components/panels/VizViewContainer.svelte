a7222e826f1b2df6ade1f8fd6cafc1f25f148f53
Les <45718863+garvageart@users.noreply.github.com>
2025-07-11 14:01:07 +0200

    add VizViewContainer component for all mounted components in a SubPanel

---
+++ viz/src/lib/components/panels/VizViewContainer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	import type { Snippet } from "svelte";
+
+	let { children, name, style }: { children: Snippet; name: string; style?: string } = $props();
+</script>
+
+<svelte:head>
+	{#if page.url.pathname !== "/"}
+		<title>{name}</title>
+	{/if}
+</svelte:head>
+<div class="viz-view-container" style="padding: 1em 3em; {style}" data-view-name={name}>
+	{@render children()}
+</div>
+
+<style>
+	.viz-view-container {
+		white-space: wrap;
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
+		overflow-y: auto;
+	}
+</style>
