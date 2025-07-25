4a669e71b502fd137d3aba9cd7ffa1ca507a773a
Les <45718863+garvageart@users.noreply.github.com>
2025-07-25 13:21:02 +0200

    fix missing syntax

--- scripts/js/updateProjectVersion.js
+++ scripts/js/updateProjectVersion.js
@@ -1,13 +1,13 @@
-import packageJSON from './package.json' assert { type: 'json' ;
-    import fs from 'fs';
+import packageJSON from './package.json' assert { type: 'json' };
+import fs from 'fs';
 
-    const newVersionNumber = process.argv[2];
+const newVersionNumber = process.argv[2];
 
-    const versionFilePath = "../../version.txt";
-    let versionFileText = Buffer.from(fs.readFileSync(versionFilePath.toString('utf-8';
+const versionFilePath = "../../version.txt";
+let versionFileText = fs.readFileSync(versionFilePath, 'utf8').split("\n")[0];
 
-    versionFileText = newVersionNumber;
-    packageJSON.version = newVersionNumber;
+versionFileText = newVersionNumber;
+packageJSON.version = newVersionNumber;
 
-    fs.writeFileSync('./package.json', JSON.stringify(packageJSON;
-    fs.writeFileSync(versionFilePath, versionFileText;
+fs.writeFileSync('./package.json', JSON.stringify(packageJSON));
+fs.writeFileSync(versionFilePath, versionFileText);
