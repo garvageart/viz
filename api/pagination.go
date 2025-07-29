edb377f72f2b09398025d9f044ecc76485f9b7be
Les <45718863+garvageart@users.noreply.github.com>
2025-07-30 00:31:15 +0200

    define pagination details

---
+++ api/pagination.go
@@ -0,0 +1,16 @@
+package api
+
+import "fmt"
+
+type Pagination struct {
+	Href   string `json:"href"`
+	Prev   string `json:"prev"`
+	Next   string `json:"next"`
+	Limit  int    `json:"limit"`
+	Offset int    `json:"offset"`
+	Count  int    `json:"count"`
+}
+
+func CreatePaginationURL(pathname string, limit, offset int) string {
+	return fmt.Sprintf("%s?limit=%d&offset=%d", pathname, limit, offset)
+}
