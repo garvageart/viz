45c85345c411a936512044c2fca0eac7c8cefecb
Les <45718863+garvageart@users.noreply.github.com>
2025-10-29 13:15:51 +0200

    chore: add dummy upload data for UI development
    - Added onMount hook with sample upload files for styling/testing
    - Created 4 dummy uploads with various progress states
    - Temporary data for UploadPanel development
    
    -- generated by Co-Pilot

--- viz/src/routes/(app)/+layout.svelte
+++ viz/src/routes/(app)/+layout.svelte
@@ -4,8 +4,49 @@
 	import UploadPanel from "$lib/components/UploadPanel.svelte";
 	import { upload } from "$lib/states/index.svelte";
 	import Notifications from "$lib/toast-notifcations/Notifications.svelte";
+	import { UploadImage, UploadState } from "$lib/upload/asset.svelte";
+	import { onMount } from "svelte";
 
 	let { children } = $props();
+
+	onMount(() => {
+		// Dummy data for styling
+		upload.files = [
+			new UploadImage({
+				filename: "mountain_sunset_4k.jpg",
+				checksum: "abc123def456",
+				data: new File([], "mountain_sunset_4k.jpg")
+			}),
+			new UploadImage({
+				filename: "beach_vacation_2024.png",
+				checksum: "ghi789jkl012",
+				data: new File([], "beach_vacation_2024.png")
+			}),
+			new UploadImage({
+				filename: "family_portrait_hires.jpg",
+				checksum: "mno345pqr678",
+				data: new File([], "family_portrait_hires.jpg")
+			}),
+			new UploadImage({
+				filename: "city_lights_night_photography.jpg",
+				checksum: "stu901vwx234",
+				data: new File([], "city_lights_night_photography.jpg")
+			})
+		];
+
+		// Set different states and progress for demo purposes
+		upload.files[0].progress = 75;
+		upload.files[0].state = UploadState.STARTED;
+
+		upload.files[1].progress = 100;
+		upload.files[1].state = UploadState.DONE;
+
+		upload.files[2].progress = 45;
+		upload.files[2].state = UploadState.STARTED;
+
+		upload.files[3].progress = 10;
+		upload.files[3].state = UploadState.STARTED;
+	});
 </script>
 
 <Header />
