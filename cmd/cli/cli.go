aabfc8cae3f7b4839c3dddb2a58e5d0f4103fa20
Les <45718863+garvageart@users.noreply.github.com>
2026-01-23 15:27:54 +0200

    chore(cli): initialise CLI go workspace

---
+++ cmd/cli/cli.go
@@ -0,0 +1,14 @@
+// This is the entry point for the Imagine CLI
+// This workspace commands such as uploading images, downloading images,
+// exporting images at different resolutions, ingesting etc.
+package main
+
+func main() {
+	println(`
+// This is the entry point for the Imagine CLI
+// This workspace commands such as uploading images, downloading images,
+// exporting images at different resolutions, ingesting etc.
+
+// For the API server, please refer to ./cmd/api/api.go
+	`)
+}
