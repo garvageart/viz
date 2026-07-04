3e9bb40d465a8a7d50217ab39f91df7a2ad10133
Les <hello@les-is.online>
2026-07-04 05:20:25 +0200

    chore: generate missing icons

---
+++ viewfinder/src/lib/components/icons/generated/IconEditCalendar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    const variants: Record<string, string> = {
+    "400": "<path d=\"M120-80v-720h120v-80h80v80h320v-80h80v80h120v280h-80v-40H200v400h280v80zm80-560h560v-80H200zm0 0v-80zM560-80v-123l263-262 123 122L683-80zm300-263-37-37zM620-140h38l121-122-18-19-19-18-122 121zm141-141-19-18 37 37z\" />"
+};
+    const filledVariants: Record<string, string> = {
+    "400": "<path d=\"M120-80v-740h125v-60h65v60h340v-60h65v60h125v320h-60v-70H180v430h320v60zm440 0v-123l263-262 122 122L683-80zm263-224 37-39-37-37-38 38z\" />"
+};
+    let { size = "1.5em", className = "", title = "edit_calendar", viewBox = "0 -960 960 960", weight = "400", fill = false, ...rest } = $props();
+    
+    const activeMap = $derived(fill ? filledVariants : variants);
+    const inner = $derived(activeMap[String(weight)] || activeMap["400"] || Object.values(activeMap)[0]);
+</script>
+
+<svg
+    class={className}
+    width={size}
+    height={size}
+    viewBox={viewBox}
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-label={title}
+    focusable="false"
+    {...rest}
+>
+    {@html inner}
+</svg>
+
+<style>
+    svg {
+        display: inline-block;
+        vertical-align: middle;
+    }
+</style>
---
+++ viewfinder/src/lib/components/icons/generated/IconPassword.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    const variants: Record<string, string> = {
+    "400": "<path d=\"M80-200v-80h800v80zm46-242-52-30 34-60H40v-60h68l-34-58 52-30 34 58 34-58 52 30-34 58h68v60h-68l34 60-52 30-34-60zm320 0-52-30 34-60h-68v-60h68l-34-58 52-30 34 58 34-58 52 30-34 58h68v60h-68l34 60-52 30-34-60zm320 0-52-30 34-60h-68v-60h68l-34-58 52-30 34 58 34-58 52 30-34 58h68v60h-68l34 60-52 30-34-60z\" />"
+};
+    const filledVariants: Record<string, string> = {
+    "400": "<path d=\"M80-200v-61h800v61zm38-254-40-22 40-68H40v-45h78l-40-68 40-22 38 67 38-67 40 22-40 68h78v45h-78l40 68-40 22-38-67zm324 0-40-24 40-68h-78v-45h78l-40-68 40-22 38 67 38-67 40 22-40 68h78v45h-78l40 68-40 24-38-67zm324 0-40-24 40-68h-78v-45h78l-40-68 40-22 38 67 38-67 40 22-40 68h78v45h-78l40 68-40 24-38-67z\" />"
+};
+    let { size = "1.5em", className = "", title = "password", viewBox = "0 -960 960 960", weight = "400", fill = false, ...rest } = $props();
+    
+    const activeMap = $derived(fill ? filledVariants : variants);
+    const inner = $derived(activeMap[String(weight)] || activeMap["400"] || Object.values(activeMap)[0]);
+</script>
+
+<svg
+    class={className}
+    width={size}
+    height={size}
+    viewBox={viewBox}
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-label={title}
+    focusable="false"
+    {...rest}
+>
+    {@html inner}
+</svg>
+
+<style>
+    svg {
+        display: inline-block;
+        vertical-align: middle;
+    }
+</style>
